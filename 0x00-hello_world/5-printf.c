07d76c70c0ff130972cb9071c847813bc8f30cbd
Joseph Siwiti <josephsiwiti@gmail.com>
2022-10-13 13:45:47 -0700

    5th commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point for program
+ *
+ * Description: 'C programming tasks'
+ *
+ * Return: Always 0 (When Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}

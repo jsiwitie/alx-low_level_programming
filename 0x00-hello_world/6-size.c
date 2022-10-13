d6e236004370acc6110478bf88724afba378faef
Joseph Siwiti <josephsiwiti@gmail.com>
2022-10-13 14:47:18 -0700

    com

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,17 +9,10 @@
 
 int main(void)
 {
-	char cha;
-	int in;
-	long int li;
-	long long int llt;
-	float ft;
-
-
-	printf("Size of a char : %d byte(s) \n", sizeof(cha));
-	printf("Size of a int : %d byte(s) \n", sizeof(in));
-	printf("Size of a long int : %d byte(s) \n", sizeof(li));
-	printf("Size of a long long int : %d byte(s) \n", sizeof(llt));
-	printf("Size of a float : %d byte(s) \n", sizeof(ft));
+	printf("Size of a char : %d byte(s) \n", sizeof(char));
+	printf("Size of a int : %d byte(s) \n", sizeof(int));
+	printf("Size of a long int : %d byte(s) \n", sizeof(long int));
+	printf("Size of a long long int : %d byte(s) \n", sizeof(long long int));
+	printf("Size of a float : %d byte(s) \n", sizeof(float));
 	return (0);
 }

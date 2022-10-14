f6976502a7c0b94a199b4475d660b623fd676776
Joseph Siwiti <josephsiwiti@gmail.com>
2022-10-13 17:01:36 -0700

    finally finally

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 /**
- * main - Entry
- * Return: Always 0
+ * main - Program's entry point
+ * Description: 'This is a c program task 6'
+ * Return: Always returns 0
  */
 
 int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
-	printf("Size of a int: %lu byte(s)\n", sizeof(int));
-	printf("Size of an long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);

319fd4e79d131fd944bc4d5aa71c0890d7e9baa6
Joseph Siwiti <josephsiwiti@gmail.com>
2022-10-14 15:15:42 -0700

    4th commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - program entry
+ * Description: 'print alphabet except q and e'
+ * Return: 0 as usual
+ */
+int main(void)
+{
+	char az = 'a';
+
+	while (az <= 'z')
+	{
+		if (az == 'e' || 'q' == az)
+			++az;
+		putchar(az);
+		++az;
+		return (0);
+	}
+}

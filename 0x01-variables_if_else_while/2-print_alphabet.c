6aa3f66d3fc76e74d0bcd2712a6375ff3c40dfca
Joseph Siwiti <josephsiwiti@gmail.com>
2022-10-14 19:13:29 -0700

    2nd commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - program entry point
+ * Description: 'prints alphabet lowercase,new line'
+ * Return: must return 0
+ */
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	return (0);
+}

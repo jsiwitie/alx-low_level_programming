77836bbf3bf8aad8f84eb6586564be11dc52de75
Joseph Siwiti <josephsiwiti@gmail.com>
2022-10-14 19:27:36 -0700

    5th commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - program entry
+ * Description: 'prints numbers'
+ * Return: returns 0
+ */
+int main(void)
+{
+	for (num = 0; num <= 10; num++)
+	{
+		putchar(num);
+	}
+	putchar('\');
+	return (0);
+}

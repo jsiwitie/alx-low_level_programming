e07602f010dc27eaccece6e5dd3b78f5f5262b1d
Joseph Siwiti <josephsiwiti@gmail.com>
2022-10-18 16:18:49 -0700

    6th com

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - program name
+ * Description: 'calculates absolute value for given integer'
+ * @t: integer for which value is to be calculated
+ * Return: the program returns absolute value
+ */
+int _abs(int t)
+{
+	if (t >= 0)
+	{
+		return (t);
+	}
+	else
+	{
+		return (-t);
+	}
+}

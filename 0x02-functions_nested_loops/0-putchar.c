e98cded56aa4466f7c7e4f3016601d5f47f1522f
Joseph Siwiti <josephsiwiti@gmail.com>
2022-10-18 11:31:12 -0700

    task 0 commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point for programme
+ * Description: 'Program that prints _putchar followed by newline'
+ * Return: returns 0
+ */
+int main(void)
+{
+	printf("_putchar \n");
+	return (0);
+}

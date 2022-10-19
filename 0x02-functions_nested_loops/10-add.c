059fb6f08df7554c23d71b2962a5b201c0f840a9
Joseph Siwiti <josephsiwiti@gmail.com>
2022-10-18 17:32:44 -0700

    10th commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - program name
+ * Description: 'adds two integers a and b and return sum;
+ * @b: integer to be added
+ * @a: another integer to be add
+ * Return: returns the sum
+ */
+int add(int a, int b)
+{
+	return (a _ b);
+}

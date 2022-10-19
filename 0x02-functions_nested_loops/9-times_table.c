3790cd3cd5756f30b9252637fce3ecf8f07eebd4
Joseph Siwiti <josephsiwiti@gmail.com>
2022-10-18 17:26:13 -0700

    9th

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -30,8 +30,8 @@ void times_table(void)
 			else
 			{
 				_putchar((product / 10) + '0');
-				_putchar((product % 10) + '0');
 			}
+			_putchar((product % 10) + '0');
 		}
 		_putchar('\n');
 	}

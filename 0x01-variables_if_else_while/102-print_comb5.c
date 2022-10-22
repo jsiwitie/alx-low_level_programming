b28c0d1f2c2ea3ff76833e652ead773391f71128
Joseph Siwiti <josephsiwiti@gmail.com>
2022-10-22 03:07:38 -0700

    task 12 commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ * Description: 'Prints all combinations of a pair of 2 #s, without repetition'
+ * Return: 0 on success.
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = 0; j <= 99; j++)
+		{
+			if (i < j && i != j)
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

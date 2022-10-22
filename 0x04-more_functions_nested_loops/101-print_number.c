7b703c8b16de259f2eb233aad827d26dd677a4d4
Joseph Siwiti <josephsiwiti@gmail.com>
2022-10-21 18:56:51 -0700

    12 commit

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_number - prototype name
+ * Description: 'prints int with putchar'
+ * @n: takes number, hello
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	unsigned int m;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		m = -n;
+
+	}
+	else
+	{
+		m = n;
+	}
+	if (m / 10 != 0)
+		print_number(m / 10);
+	_putchar((m % 10) + '0');
+}

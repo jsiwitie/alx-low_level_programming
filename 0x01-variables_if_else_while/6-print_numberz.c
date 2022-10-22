cd087c2116a7cf5355fd722125d03cd110ab6721
Joseph Siwiti <josephsiwiti@gmail.com>
2022-10-22 01:34:45 -0700

    task 6 commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ * Description - 'use variable int,print numbers 0 to 9'
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+
+	int c;
+
+	c = 0;
+
+	while ('0' + c <= '9')
+	{
+
+		putchar('0' + c);
+		c++;
+
+	}
+
+	putchar('\n');
+	return (0);
+}

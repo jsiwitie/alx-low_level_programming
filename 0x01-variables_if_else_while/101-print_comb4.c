3ea51a59f6a1d0441757096bc2eb60de4aecca44
Joseph Siwiti <josephsiwiti@gmail.com>
2022-10-22 02:29:14 -0700

    task 11 commit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ * Description - 'nested loop print combo of 3 numbers'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int hundreds;
+
+	int tens;
+
+	int ones;
+
+	for (hundreds = 0; hundreds <= 9; hundreds++)
+	{
+		for (tens = hundreds + 1; tens <= 9; tens++)
+		{
+			for (ones = tens + 1; ones <= 9; ones++)
+			{
+				putchar(hundreds + '0');
+				putchar(tens + '0');
+				putchar(ones + '0');
+				if (hundreds < 7)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar ('\n');
+	return (0);
+}

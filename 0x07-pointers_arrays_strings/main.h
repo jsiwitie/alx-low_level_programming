1bcab6085ed883c5948b7ba4b7eafa96ead20a79
jsiwitie <josephsiwiti@gmail.com>
2022-11-28 02:57:46 -0800

    first commit

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+/* begining of project prototypes*/
+unsigned int _strspn(char *s, char *accept);
+char *_memset(char *s, char b, unsigned int n);
+void print_diagsums(int *a, int size);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+char *_strpbrk(char *s, char *accept);
+void print_chessboard(char (*a)[8]);
+char *_strstr(char *haystack, char *needle);
+int _putchar(char c);
+void set_string(char **s, char *to);
+/*end of prototypes*/
